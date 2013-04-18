f2fe5539f253127ac470e26e355c4258170cb4f7
Wolfgang Schnerring <wosc@wosc.de>
2013-04-18 12:01:36 +0200

    Back to development: 1.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.testing',
-    version='1.7',
+    version='1.8.dev0',
     author='Wolfgang Schnerring <ws at gocept dot com>',
     author_email='ws@gocept.com',
     url='https://code.gocept.com/hg/public/gocept.testing',
