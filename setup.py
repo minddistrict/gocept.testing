faa4671f5e5a075196e43933af534d367b53300a
Steffen Allner <sa@gocept.com>
2017-11-29 09:54:07 +0100

    Drop support for Python 3.3 and add it for 3.6.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.testing',
-    version='1.12.dev0',
+    version='2.0.dev0',
     author='gocept <mail at gocept dot com>',
     author_email='mail@gocept.com',
     url='https://bitbucket.org/gocept/gocept.testing',
@@ -21,9 +21,9 @@ A collection of test helpers, additional assertions, and the like.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development',
